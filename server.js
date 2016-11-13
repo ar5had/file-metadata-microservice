94f0b6f2b2dfaa1585a9b6a410966477ed90a8e5
Arshad Khan <arshdkhn1@gmail.com>
2016-11-13 01:05:03 +0000

    final push

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var express = require("express")
     , favicon = require("serve-favicon")
     , port = process.env.PORT || 8080;
 
-dotenv.load();    
+dotenv.load();
+var upload = multer();
 var app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
@@ -15,6 +16,15 @@ app.set("view-engine", "ejs");
 
 app.get('/getmetadata', function(req, res) {
     res.render('pages/getmd.ejs');
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html")); 
+});
+
+app.post("/", upload.single("uploadedFile"), function(req,res) {
+   var data = JSON.stringify({"size": req.file.size});
+   res.render("pages/metadata.ejs", {data: data}); 
 });
 
 app.listen(port, function(){
